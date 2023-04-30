163ff35f8a2b83e563da0173d94205c5fd5c7dd5
monjurctg <monjurpy.06@gmail.com>
2023-05-01 00:16:43 +0600

    update

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default function App() {
         {/* <BookingSlider /> */}
         {/* <InputMobile /> */}
 
-        <TextInputCustom />
+        <TextInputCustom label={"Phone "} />
         <TextInputCustom />
         {/* <CalenderCustom /> */}
 
